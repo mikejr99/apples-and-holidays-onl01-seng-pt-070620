8e1540af2441c42cf658645bed42dba687adc197
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-19 11:19:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,10 +31,7 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-holiday_hash[:spring][:memorial_day] << "Grill"
-holiday_hash
-#holiday_hash[:spring][:memorial_day] << "Table Cloth"
-holiday_hash
+holiday_hash[:spring][:memorial_day] << supply
 
 end
 
