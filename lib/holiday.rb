caca2a62141873735ecde2d2ece9e8f58beee2ff
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-19 11:30:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,9 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:Winter].values
+  holiday_hash[:Winter].each do |holiday, supplies|
+    supplies.flatten
+  end
 binding.pry
 end
 
